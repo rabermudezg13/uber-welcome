1bec90b72e3c77f430d1958d92bcb048ed782c13
rabermudezg13 <rabermudezg@gmail.com>
2026-06-10 10:55:30 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,22 @@
-// STEP 1: Paste your Firebase Web App config here.
-// Firebase Console > Project Settings > General > Your apps > Web app
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "PASTE_YOUR_PROJECT",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC64UVzIcLRgXqgA7Q_xeyo4Q8KN97Uku0",
+  authDomain: "uber-welcome.firebaseapp.com",
+  databaseURL: "https://uber-welcome-default-rtdb.firebaseio.com",
+  projectId: "uber-welcome",
+  storageBucket: "uber-welcome.firebasestorage.app",
+  messagingSenderId: "80473458619",
+  appId: "1:80473458619:web:be246c62fb2dc826dda69e",
+  measurementId: "G-ZKHHSTLL2D"
 };
 
-// Your Spotify Jam link from the QR you sent.
-export const spotifyJamLink = "https://spotify.link/FOVMFGmjR3b";
-
-// Optional driver name shown on tablet.
-export const driverName = "Rodrigo";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
